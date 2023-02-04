05e8293ea483838a9535386cc550f0e0002f82d2
Sohini Bhattacharya <bhattacharyasohini48@gmail.com>
2023-02-04 23:52:27 +0530

    Added Authentication

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -6,11 +6,19 @@ export default function RegisterPage(){
   const [password,setPassword]=useState('');
   async function register(ev){
       ev.preventDefault();
-      await fetch('http://localhost:4000/register',{
+
+     const response=await fetch('http://localhost:4000/register',{
         method: 'POST',
         body: JSON.stringify({username,password}),
         headers: {'Content-Type':'application/json'},
-      })
+      });
+      if(response.status !== 200)
+      {
+        alert('registration failed');
+      }
+    else{
+          alert('registration suceeded');
+    }
   }
   return(
     <form className="register" onSubmit={register}>
