8045c4535ddc2f037d8fc1949522cd51091c23d4
Sohini Bhattacharya <bhattacharyasohini48@gmail.com>
2023-02-06 00:04:07 +0530

    Added CreatePost page and configured the reqd apis

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,25 @@
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
 
-export default function Post() {
-  return(
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
+  return (
     <div className="post">
-          <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"></img>
-          </div>
-          <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <span className ="author">Sohini B</span>
-          <time>2023-02-02 21:20</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
-     </div>
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
       </div>
-)}
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
